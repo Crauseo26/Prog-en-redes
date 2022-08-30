464fb95acbf2439a4afacf3aac01c16cd38bd0b6
crauseo <carlos.rauseo@innovant.us>
2022-08-30 19:45:10 -0300

    [TaskPattern]

---
+++ TaskPatternDelegate/ConcreteTask.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskPatternDelegate
+{
+    internal class ConcreteTask : AbstractTask
+    {
+        protected override void Process()
+        {
+            Console.WriteLine("Starting concrete Task!");
+            for (var i = 0; i < 20; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Running Task iteration: " + i);
+            }
+
+            Console.WriteLine("Finishing concrete Task!");
+        }
+    }
+}
